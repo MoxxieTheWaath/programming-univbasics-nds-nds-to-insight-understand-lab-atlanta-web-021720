ad95a293f37be22784f89b849960b66f9fef5938
Michael McDonald <mchlmcdnld7@gmail.com>
2020-02-17 09:23:45 +0000

    Automatically backed up by Learn

--- lib/nds_explore.rb
+++ lib/nds_explore.rb
@@ -9,7 +9,7 @@ def pretty_print_nds(nds)
 end
 
 def print_first_directors_movie_titles
-  movieTitle = pretty_print_nds(directors_database)
-  abo = movieTitle[0][:movies]
+ # movieTitle = pretty_print_nds(directors_database)
+  abo = directors_database[0][:movies]
   print pretty_print_nds(abo)
 end
